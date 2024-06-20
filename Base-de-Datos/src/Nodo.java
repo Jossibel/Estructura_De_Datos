586071b292b41f65c2fe3ad220e90f2b919ca552
Isauro Rivera <isauro.rivera@unl.edu.ec>
2024-06-19 21:46:48 -0500

    Create Nodo.java

---
+++ Base-de-Datos/src/Nodo.java
@@ -0,0 +1,38 @@
+public class Nodo {
+    private Object valor;
+    private Nodo izquierda;
+    private Nodo derecha;
+
+    // Constructor
+    public Nodo(Object valor) {
+        this.valor = valor;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+
+    // Getters y setters
+    public Object getValor() {
+        return valor;
+    }
+
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+}
+
