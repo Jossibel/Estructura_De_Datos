90c317918eecb3609d3f323fd15d86affcc5008d
mechm <maria.chuico@unl.edu.ec>
2024-06-20 06:18:51 -0500

    Correcion  a Clase EstructuradeDato
    20/06/2024

--- Base-de-Datos/src/EstructuraDeDatos.java
+++ Base-de-Datos/src/EstructuraDeDatos.java
@@ -1,15 +1,4 @@
-public abstract class EstructuraDeDatos {
-
-    // Método abstracto para agregar un valor
-    public abstract void agregar(Object valor);
-
-    // Método abstracto para insertar un valor
-    public abstract void insertar(Object valor);
-
-    // Método abstracto para eliminar un valor
-    public abstract void eliminar(Object valor);
-
-    public abstract void POSORDEN();
-
-    public abstract void PREORDEN();
+public interface EstructuraDeDatos {
+    
+    public boolean agregar(int valor);
 }
